e47486d657358ca9f9a11d231f0aa10a4f95b0e9
Ximena <ximenarubio121@gmail.com>
2026-03-25 17:11:41 -0500

    se realizo el primer requisito funcional 1: se agrega conexion MySQL con db.js, .env y actualizacion de app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express'; // Importar el framework express para el servidor
+import 'dotenv/config'; // Importar dotenv para manejar variables de entorno
+import './config/db.js'; // Importar la configuración de la base de datos para establecer la conexión
 import rutasUsuarios from './routes/user.routes.js'; // Importar el enrutador de usuarios
 import rutasTareas   from './routes/tasks.routes.js'; // Importar el enrutador de tareas
 import rutasAuth     from './routes/auth.routes.js'; // Importar el enrutador de autenticación
 
 const app = express(); // Instanciar la aplicación express
-const puerto = 3000; // Asignar el número del puerto en el que correrá el servidor
+// Configuración del puerto para el servidor, utilizando una variable de entorno o un valor por defecto
+const puerto = process.env.PORT || 3000; // Asignar el número del puerto en el que correrá el servidor
 
 app.use(express.json()); // Habilitar el middleware para entender archivos JSON
 
---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+pool.getConnection()
+    .then(connection => {
+        console.log('Conexión a la base de datos MySQL establecida con éxito');
+        connection.release();
+    })
+    .catch(error => {
+        console.error('Error al conectar con la base de datos:', error.message);
+    });

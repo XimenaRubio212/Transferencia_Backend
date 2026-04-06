b261ab94ba6f92443334576ef4de2b61721ddde1
ximena <ximenarubio121@gmail.com>
2026-04-06 17:03:25 -0500

    Se realizo cambios en la base de datos

--- sql/database.sql
+++ sql/database.sql
@@ -32,19 +32,63 @@ priority varchar(20) default 'medio',
 userId int,
 dueDate datetime null,
 createdAt timestamp default current_timestamp,
-foreign key (userId) references users(id) on delete set null
+foreign key (userId) references users(id) on delete restrict
 );
 
 -- Insertar usuarios de JSON
-insert into users (id, documento, nombre, email) values 
+insert into users (id, documento, nombre, email) values
 (1, '10203040', 'Juan Pérez', 'juan.perez@email.com'),
 (2, '50607080', 'Maria Garcia', 'm.garcia@email.com'),
 (3, '11223344', 'Carlos López', 'c.lopez@email.com');
 
 -- Insertar tareas relacionadas con los IDs 1, 2 y 3
-insert into tasks (title, description, estado, priority, userId) values 
+insert into tasks (title, description, estado, priority, userId) values
 ('Revisar Servidor', 'Verificar la conexión de la base de datos', 'pendiente', 'alta', 1),
 ('Actualizar Frontend', 'Cambiar los colores del dashboard', 'en progreso', 'media', 2),
 ('Documentar API', 'Escribir los endpoints en el README', 'completado', 'baja', 3);
+
+-- este es para consultar un usuario
+SELECT nombre, email
+FROM users
+WHERE documento = '10203040';
+
+-- este es para una tarea con su descripcion, estado y prioridad
+SELECT t.title, t.description, t.estado, t.priority
+FROM tasks t
+JOIN users u ON t.userId = u.id
+WHERE u.documento = '10203040';
+
+-- este es para consultar una tarea y con su usuario
+SELECT t.id, t.title, t.description, t.estado, t.priority, u.nombre AS usuario_asignado
+FROM tasks t
+LEFT JOIN users u ON t.userId = u.id
+WHERE t.id = 6;
+
+-- este es para buscar una tarea en especifico y a que usuario esta asignada
+SELECT t.id, t.title, t.description, t.estado, t.priority, u.nombre AS usuario_asignado
+FROM tasks t
+LEFT JOIN users u ON t.userId = u.id
+WHERE t.title = 'Terminar proyecto del CRUD con base de datos';
+
+-- este es para que muestre el total de tareas de cada usuario y sus tareas asignadas
+SELECT u.nombre, u.documento,
+       COUNT(t.id) AS total_tareas,
+       GROUP_CONCAT(t.title SEPARATOR ' | ') AS tareas_asignadas
+FROM users u
+LEFT JOIN tasks t ON t.userId = u.id
+GROUP BY u.id, u.nombre, u.documento;
+
+-- este es para que muestre todas las tareas de todos los usuarios alfabeticamente
+SELECT u.nombre, t.title, t.estado, t.priority
+FROM users u
+LEFT JOIN tasks t ON t.userId = u.id
+ORDER BY u.nombre;
+
+-- este es para ver cuantas tareas asociada tiene cada usuario
+SELECT u.nombre, u.documento,
+       COUNT(t.id) AS total_tareas
+FROM users u
+LEFT JOIN tasks t ON t.userId = u.id
+GROUP BY u.id, u.nombre, u.documento;
 
 show grants for 'tasks_user'@'localhost';
